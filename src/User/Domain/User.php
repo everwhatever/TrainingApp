561c021413cdf7b634353dd1cc40a53dfd3b5350
everwhatever <koszykarz.kuba@gmail.com>
2025-01-02 15:19:51 +0100

    Create user bounded context and user model

---
+++ src/User/Domain/User.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Domain;
+
+use Symfony\Component\Uid\Uuid;
+
+class User
+{
+    public function __construct(
+        private readonly Uuid $uuid,
+        private readonly string $email,
+        private string $password,
+        private readonly string $firstName,
+        private readonly string $lastName
+    )
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('Invalid email address.');
+        }
+
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function getUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getFullName(): string
+    {
+        return "{$this->firstName} {$this->lastName}";
+    }
+
+    public function changePassword(string $password): void
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+}
